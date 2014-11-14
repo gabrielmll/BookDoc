4c525ea0c6acd93acda7280561a130f1b9eee8ba
Gabriel Pires e Albuquerque de Mello <gabrielmll@gmail.com>
2014-11-14 19:04:43 -0200

    Gestão de Listas e sua comunicação com Sistema

--- src/Sistema/Listas.java
+++ src/Sistema/Listas.java
@@ -13,24 +13,38 @@ public class Listas {
 	private static ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
 	private static ArrayList<Consulta> consultas = new ArrayList<Consulta>();
 	private static ArrayList<Exame> exames = new ArrayList<Exame>();
-	
+
 	public static ArrayList<Pessoa> getPessoas() {
 		return pessoas;
 	}
+
 	public static void setPessoas(ArrayList<Pessoa> pessoas) {
 		Listas.pessoas = pessoas;
 	}
+
 	public static ArrayList<Consulta> getConsultas() {
 		return consultas;
 	}
+
 	public static void setConsultas(ArrayList<Consulta> consultas) {
 		Listas.consultas = consultas;
 	}
+
 	public static ArrayList<Exame> getExames() {
 		return exames;
 	}
+
 	public static void setExames(ArrayList<Exame> exames) {
 		Listas.exames = exames;
 	}
-	
+
+	public void addOject(Object obj) {
+		if (obj instanceof Pessoa)
+			getPessoas().add((Pessoa) obj);
+		else if (obj instanceof Consulta) {
+			getConsultas().add((Consulta) obj);
+		} else if (obj instanceof Exame) {
+			getExames().add((Exame) obj);
+		}
+	}
 }
--- src/Sistema/Sistema.java
+++ src/Sistema/Sistema.java
@@ -9,9 +9,26 @@ package Sistema;
  */
 public class Sistema {
 	private Clinica clinica;
-	private Listas listas;	// listas contem Pessoas, Exames e Consultas
-	
-	Sistema(String nomeClinica){
-		clinica = new Clinica(nomeClinica);
+	private Listas listas; // listas contem Pessoas, Exames e Consultas
+
+	Sistema(String nomeClinica) {
+		setClinica(new Clinica(nomeClinica));
 	}
+
+	public Listas getListas() {
+		return listas;
+	}
+
+	public void setListas(Listas listas) {
+		this.listas = listas;
+	}
+
+	public Clinica getClinica() {
+		return clinica;
+	}
+
+	public void setClinica(Clinica clinica) {
+		this.clinica = clinica;
+	}
+
 }
