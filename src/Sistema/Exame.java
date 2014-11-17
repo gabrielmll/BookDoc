78284b3612697803c3215b44a560a5740e45acf9
Gabriel Pires e Albuquerque de Mello <gabrielmll@gmail.com>
2014-11-17 16:40:49 -0200

    Finalizado a gestão de listas e suas comunicações com o sistema

--- src/Sistema/Exame.java
+++ src/Sistema/Exame.java
@@ -10,8 +10,12 @@ package Sistema;
 public class Exame {
 	private Agenda agenda;
 	private Cliente cliente;
-	
-	public Exame(Cliente cliente){
+
+	public Exame(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	public String toString() {
+		return "Cliente " + cliente.toString() + " realiza exame na data ";
 	}
 }
