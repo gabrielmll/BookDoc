0811d89aee18bd1819224dd122f4d8b708681765
Gabriel Pires e Albuquerque de Mello <gabrielmll@gmail.com>
2014-11-18 20:14:26 -0200

    Criada tela de login

--- src/GUI/JanelaPadrao.java
+++ src/GUI/JanelaPadrao.java
@@ -3,10 +3,15 @@ package GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class JanelaPadrao {
 
 	private JFrame frame;
+	private JTextField senhaLogin;
+	private JTextField idLogin;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,33 @@ public class JanelaPadrao {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 900, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		//telaLogin();
 	}
 
+	private void telaLogin() {
+
+		JLabel lblId = new JLabel("ID:");
+		lblId.setBounds(343, 216, 46, 14);
+		frame.getContentPane().add(lblId);
+
+		JLabel lblSenha = new JLabel("SENHA:");
+		lblSenha.setBounds(343, 247, 46, 14);
+		frame.getContentPane().add(lblSenha);
+
+		senhaLogin = new JTextField();
+		senhaLogin.setBounds(400, 244, 86, 20);
+		frame.getContentPane().add(senhaLogin);
+		senhaLogin.setColumns(10);
+
+		idLogin = new JTextField();
+		idLogin.setBounds(400, 213, 86, 20);
+		frame.getContentPane().add(idLogin);
+		idLogin.setColumns(10);
+
+		JButton entrarLogin = new JButton("Entrar");
+		entrarLogin.setBounds(399, 275, 89, 23);
+		frame.getContentPane().add(entrarLogin);
+	}
 }
